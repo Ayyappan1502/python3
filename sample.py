515aad71ddac587f1331e1a454ff3a4843dc1d54
Ayyappan1502 <srayyappan05@gmail.com>
2025-04-26 21:29:37 +0530

    upadate sample file

--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
+#! usr/bin/python3
+
 print("git hub is updated successfully :)")
+import os
+#execute the command through os module in popen() ,
+# therefore it simply read the output from cmd prompt 
+# And deliver the output via python exection , that is happened at behind the scene.
+output = os.popen('ls').read()
+print(output)
